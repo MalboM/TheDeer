8a87cbfdd3e59197761312e1248b836786edb4c3
Fabio Ferrara <fabioferrara1989@gmail.com>
2015-11-20 21:28:29 +0100

    cambiement

---
+++ Assets/Scripts/PadDrag.js
@@ -0,0 +1,61 @@
+﻿import System.Collections.Generic;
+
+var i : float;
+var t : float;
+var prova : float;
+
+var translationvertical : float;
+var translationhorizontal : float;
+
+var speed : float;
+
+var limitey : float;
+var limitey2 : float;
+
+function Update () {
+			
+	
+	
+	this.transform.Translate(Vector3.up *i* -2* Time.deltaTime, Space.World);
+	this.transform.Translate(Vector3.right *t*2* Time.deltaTime, Space.World);
+	
+	
+
+	if ((Input.GetAxisRaw("RightJoystickVertical") <limitey) || (Input.GetAxisRaw("RightJoystickVertical") >limitey2) )
+		
+       	 {
+            prova = 1;
+            i = Input.GetAxisRaw ("RightJoystickVertical") ;
+             
+    }
+    else{
+    
+    prova = 0;
+    i = 0;
+    }
+    
+    
+    
+    if ((Input.GetAxisRaw("RightJoystickHorizontal") <limitey) || (Input.GetAxisRaw("RightJoystickHorizontal") >limitey2) )
+		
+       	 {
+            prova = 1;
+           	t = Input.GetAxisRaw ("RightJoystickHorizontal") ;	
+	
+             
+    }
+    else{
+    
+    prova = 0;
+    t = 0;
+    }
+    
+    
+//    if (Input.GetAxis("RightJoystickHorizontal"))
+//        {
+//             t = 1;
+//             
+//    }
+//       else {t = 0;}
+//    
+}
