86718b39f0c21e46db6bc519517a7b9fb05afcec
Fabio Ferrara <fabioferrara1989@gmail.com>
2015-11-27 15:50:54 +0100

    scenes

---
+++ Assets/Scripts/ChangeSceneAfterSeconds.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+
+var scena : int;
+
+function Start () {
+
+    StartCoroutine(WaitandLoad(5.0)) ;
+        
+}
+
+
+
+function WaitandLoad (waitTime : float) {
+  
+    yield WaitForSeconds (waitTime);
+       Application.LoadLevel(scena);
+}
