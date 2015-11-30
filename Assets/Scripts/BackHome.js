b622378ad66c348efd86ca3300eaba9c22a62dd5
Fabio Ferrara <fabioferrara1989@gmail.com>
2015-11-30 18:32:35 +0100

    FinalBuild

--- Assets/Scripts/BackHome.js
+++ Assets/Scripts/BackHome.js
@@ -1,6 +1,19 @@
 
 function OnMouseDown () {
 
-	Application.LoadLevel(0);
+	Application.LoadLevel(1);
 	
+}
+
+
+
+function OnTriggerStay(other:Collider){
+    
+    if(Input.GetAxis ("PadA") == 1) {
+        Application.LoadLevel(1);
+    }
+    
+    if(Input.GetAxis("PadRightTrigger") >= 0.1){
+        Application.LoadLevel(1);
+    }
 }
---
+++ Assets/Scripts/OnMouseEnterAnimation.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var animazione : GameObject;
+
+
+
+function OnMouseEnter () {
+    animazione.GetComponent.<Animation>().Play();
+}
+
+
+
+
+
+function OnTriggerEnter(other:Collider){
+    
+    animazione.GetComponent.<Animation>().Play();
+}
